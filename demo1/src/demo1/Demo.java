f41f53efff7e63e6b4119a8853c13283be8e0ece
sai <sai@DESKTOP-NGO2U3H>
2025-08-07 10:30:05 +0530

    Dayone

---
+++ demo1/src/demo1/Demo.java
@@ -0,0 +1,17 @@
+package demo1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		//widening //implicit type casting
+		byte b=10;
+		int l=b;
+		System.out.println(l);
+		float f=22.14f;
+		double d=f;
+		System.out.println(d);
+		
+	}
+
+}
+ 
